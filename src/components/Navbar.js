6f9e5d6dc1f5bbb8f53526aaa7f8bab7ac2accca
adeliaaaa <monicadelia88@gmail.com>
2023-09-28 21:17:35 +0700

    feat: add working navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,44 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { Link, useLocation } from "react-router-dom";
+import Logo from "../assets/Logo.svg";
+import "../styles/Navbar.css";
 
 function Navbar() {
+	const location = useLocation();
+
+	useEffect(() => {
+		// console.log("Location changed", location);
+
+		const links = document.querySelectorAll(".right-navbar a");
+		links.forEach((link) => {
+			link.classList.remove("active");
+			if (link.id === location.pathname) link.classList.add("active");
+		});
+	}, [location]);
+
 	return (
 		<>
 			<nav className="navbar">
-				<div className="nav-wrapper">Navbar</div>
+				<div className="left-navbar">
+					<img src={Logo} alt="logo" border="0" width="64px" height="64px" />
+				</div>
+				<div className="right-navbar">
+					<Link to="/" id="/">
+						Beranda
+					</Link>
+					<Link to="/beli" id="/beli">
+						Beli
+					</Link>
+					<Link to="/bus" id="/bus">
+						Bus
+					</Link>
+					<Link to="/rute" id="/rute">
+						Rute
+					</Link>
+					<Link to="/jadwal" id="/jadwal">
+						Jadwal
+					</Link>
+				</div>
 			</nav>
 		</>
 	);
