fb14d773a4bc8f65727cb2a13866b9ab679a5195
adeliaaaa <monicadelia88@gmail.com>
2023-09-30 23:30:23 +0700

    feat: add section menu in beranda page

--- src/pages/Beranda.js
+++ src/pages/Beranda.js
@@ -1,8 +1,13 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Footer from "../components/Footer";
 import BusImage from "../assets/Home-Bus.png";
 import LocationImage from "../assets/Location.png";
 import SearchImage from "../assets/Search.png";
+import InfoBusImage from "../assets/InfoBus.png";
+import RuteBusImage from "../assets/RuteBus.png";
+import LiveBusImage from "../assets/LiveBus.png";
+import BantuanBusImage from "../assets/BantuanBus.png";
 import "../styles/Beranda.css";
 
 function Beranda() {
@@ -39,7 +44,27 @@ function Beranda() {
 					<img src={BusImage} alt="Bus" height="285px" />
 				</div>
 			</div>
-			<h3>menu</h3>
+			<div className="menu-section">
+				<h2>Menu</h2>
+				<div className="menu-feature-container">
+					<Link to="/bus" className="card-container">
+						<img src={InfoBusImage} alt="Info Bus" height="100px" />
+						<div className="text1">Info Bus</div>
+					</Link>
+					<Link to="/rute" className="card-container">
+						<img src={RuteBusImage} alt="Info Bus" height="100px" />
+						<div className="text1">Rute</div>
+					</Link>
+					<Link to="/live" className="card-container">
+						<img src={LiveBusImage} alt="Info Bus" height="100px" />
+						<div className="text1">Live Bus</div>
+					</Link>
+					<Link to="/bantuan" className="card-container">
+						<img src={BantuanBusImage} alt="Info Bus" height="100px" />
+						<div className="text1">Bantuan</div>
+					</Link>
+				</div>
+			</div>
 			<h3>fitur</h3>
 			<h3>stay update </h3>
 			<Footer />
