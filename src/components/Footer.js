ebc36a2f9c70713966f28e83f2dd0f1ac76209d5
adeliaaaa <monicadelia88@gmail.com>
2023-09-30 22:52:49 +0700

    feat: add explore section

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,23 +8,21 @@ function Footer() {
 				<div className="left-side">
 					<div className="contact-us-text"> CONTACT US</div>
 					<div className="info">
-						<p>
-							Phone (022) 123 456 <br></br> Fax (022) 123 456
-						</p>
-
-						<p>
-							Jalan Ganesa No. 202 <br></br>Kota Bandung, Jawa Barat
-						</p>
-
-						<p>contactus@biskini.com</p>
+						<div className="text2">Phone (022) 123 456</div>
+						<div className="text2">Fax (022) 123 456</div>
+						<br></br>
+						<div className="text2">Jalan Ganesa No. 202</div>
+						<div className="text2">Kota Bandung, Jawa Barat</div>
+						<br></br>
+						<div className="text2">contactus@biskini.com</div>
 					</div>
 				</div>
 				<div className="right-side">
 					<div className="contact-us-text"> SARAN </div>
-					<div className="saran-text">
+					<div className="saran-text text1">
 						Saran dan masukan Anda untuk perbaikan kami
 					</div>
-					<div className="saran-field"> 
+					<div className="saran-field">
 						<input type="text" id="nama" name="nama" placeholder="Nama"></input>
 						<input
 							type="text"
--- src/pages/Beranda.js
+++ src/pages/Beranda.js
@@ -1,18 +1,47 @@
 import React from "react";
 import Footer from "../components/Footer";
+import BusImage from "../assets/Home-Bus.png";
+import LocationImage from "../assets/Location.png";
+import SearchImage from "../assets/Search.png";
+import "../styles/Beranda.css";
 
 function Beranda() {
 	return (
 		<>
-			<h1>Berandaaaaaaaaaaaaaaaa</h1>
-			<h1>Berandaaaaaaaaaaaaaaaa</h1>
-			<h1>Beranda</h1>
-			<h1>Beranda</h1>
-			<h1>Beranda</h1>
-			{/* <div>Explore</div>
-			<div>Menu</div>
-			<div>Kenali fitur yang tersedia</div>
-			<div>Stay update</div> */}
+			<div className="explore-section">
+				<div className="left-side-explore">
+					<div>
+						<h1 className="explore-heading-color1">EXPLORE THE</h1>
+						<h1 className="explore-heading-color2">ADVENTOROUS LIFE</h1>
+					</div>
+					<div className="explore-description text2">
+						Temukan lokasi tujuanmu dan kami akan membantu merekomendasikan rute
+						terbaik untukmu
+					</div>
+					<div className="explore-search-container">
+						<div className="explore-search-input">
+							<img src={LocationImage} alt="Location" height="24px" />
+							<input
+								type="text"
+								id="location"
+								name="location"
+								placeholder="Mau Kemana?"
+							></input>
+						</div>
+						<img
+							src={SearchImage}
+							alt="Search"
+							className="explore-search-button"
+						/>
+					</div>
+				</div>
+				<div className="right-side-explore">
+					<img src={BusImage} alt="Bus" height="285px" />
+				</div>
+			</div>
+			<h3>menu</h3>
+			<h3>fitur</h3>
+			<h3>stay update </h3>
 			<Footer />
 		</>
 	);
