66ddd9c6248a4fe04ac9338e53b5e0cdc60c143a
adeliaaaa <monicadelia88@gmail.com>
2023-10-15 22:06:23 +0700

    feat: add title section

--- src/pages/Bus.js
+++ src/pages/Bus.js
@@ -1,9 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
+import DropDownButton from "../assets/InfoBusDropDown.png";
+import SearchButton from "../assets/InfoBusSearch.png";
+import "../styles/Bus.css";
 
 function Bus() {
+	const [isDropDownOpen, setIsDropDownOpen] = useState(false);
+
+	const dropdownClicked = () => {
+		setIsDropDownOpen(!isDropDownOpen);
+	};
+
 	return (
 		<>
-			<h1>Bus</h1>
+			<div className="title-section">
+				<h1>
+					Informasi <span>Bus</span>
+				</h1>
+				<div className="text1">Temukan informasi bus favoritmu</div>
+				<div className="title-dropdown">
+					<div className="dropdown-container text2">
+						<div className="dropdown-text" onClick={dropdownClicked}>
+							<span>Pilih Agensi Bus</span>
+							<img src={DropDownButton} alt="dropdown" height={24} />
+						</div>
+						{isDropDownOpen && (
+							<ul className="dropdown-lists">
+								<li className="dropdown-list">Agensi 1</li>
+								<li className="dropdown-list">Agensi 2</li>
+								<li className="dropdown-list">Agensi 3</li>
+								<li className="dropdown-list">Agensi 4</li>
+								<li className="dropdown-list">Agensi 5</li>
+							</ul>
+						)}
+					</div>
+					<div className="dropdown-search">
+						<button className="dropdown-search-button">
+							Cari
+							<img src={SearchButton} alt="search" height={24} />
+						</button>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
