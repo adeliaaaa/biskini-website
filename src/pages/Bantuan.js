3729ca9278f3ffa8fcfabaf3c0f6efe1b8dea711
adeliaaaa <monicadelia88@gmail.com>
2023-10-16 18:26:05 +0700

    feat: add bantuan page

--- src/pages/Bantuan.js
+++ src/pages/Bantuan.js
@@ -1,9 +1,40 @@
 import React from "react";
+import "../styles/bantuan.css";
 
 function Bantuan() {
 	return (
 		<>
-			<h1>Bantuan</h1>
+			<div className="bantuan-page">
+				<div className="right-side">
+					<div className="contact-us-text"> BANTUAN </div>
+					<div className="saran-text text1">
+						Sampaikan Keluhan Anda kepada Kami
+					</div>
+					<div className="saran-field">
+						<input type="text" id="nama" name="nama" placeholder="Nama"></input>
+						<input
+							type="text"
+							id="email"
+							name="Nomor Telepon"
+							placeholder="Nomor Telepon"
+						></input>
+						<input
+							type="text"
+							id="email"
+							name="email"
+							placeholder="Email"
+						></input>
+						<textarea
+							type="text"
+							id="saran"
+							name="keluhan"
+							placeholder="Keluhan"
+							className="saran-input"
+						></textarea>
+						<button>Kirim</button>
+					</div>
+				</div>
+			</div>
 		</>
 	);
 }
