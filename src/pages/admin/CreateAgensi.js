c7b662f1eddabadb721aec8809934c562759801c
Adelline Kania <adellinekaniasetiyawan@gmail.com>
2023-10-16 13:22:45 +0700

    add admin pages template

---
+++ src/pages/admin/CreateAgensi.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreateAgensi() {
+	return (
+		<>
+			<h1>CreateAgensi</h1>
+		</>
+	);
+}
+
+export default CreateAgensi;
---
+++ src/pages/admin/EditBus.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function EditBus() {
+	return (
+		<>
+			<h1>EditBus</h1>
+		</>
+	);
+}
+
+export default EditBus;
